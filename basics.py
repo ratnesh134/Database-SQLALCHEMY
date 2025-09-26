fc5fee1d90e6597b46234251ca9800401994169e
Ratnesh Kumar <ratnesh134@gmail.com>
2025-09-26 15:12:31 +0530

    SQLAlchemy orm sessions

--- basics.py
+++ basics.py
@@ -7,3 +7,11 @@ conn  = engine.connect()
 conn.execute(text("CREATE TABLE IF NOT EXISTS people (name str, age int)"))
 
 conn.commit()
+
+from sqlalchemy.orm import Session
+
+session = Session(engine)
+
+session.execute(text('INSERT INTO people (name, age) VALUES ("MIKE",30);'))
+
+session.commit()
